619d6d1b88451bc747999d6b5912d105af53be4d
Luis <luisbsc_1996@hotmail.com>
2020-04-20 07:14:53 -0500

    Se sube imagenes pero no funciona editar

--- src/app/_service/perfil.service.ts
+++ src/app/_service/perfil.service.ts
@@ -6,6 +6,7 @@ import { AngularFireAuth } from '@angular/fire/auth';
 import { switchMap, map, finalize } from 'rxjs/operators';
 import { AngularFireStorage } from '@angular/fire/storage';
 import { FileI } from '../_model/imagenes';
+import { LoginService } from './login.service';
 
 @Injectable({
   providedIn: 'root'
@@ -14,11 +15,13 @@ export class PerfilService {
 
   // Variable para validar el estado del usuario
   user: Observable<Perfil>;
-  // private perfilCollection: AngularFirestoreCollection<Perfil>;
-  // private filePath: any;
-  // private UrlImagen: Observable<string>;
-  
-  constructor(private afs: AngularFirestore, private afa: AngularFireAuth, private storage: AngularFireStorage) { 
+   private perfilCollection: AngularFirestoreCollection<Perfil>;
+   private filePath: any;
+   private UrlImagen: Observable<string>;
+   usuarioLogeado: string;
+   
+
+  constructor(private afs: AngularFirestore, private afa: AngularFireAuth, private storage: AngularFireStorage, private loginService: LoginService) { 
     this.user = this.afa.authState.pipe(
       switchMap( user => {
         if(user){
@@ -28,8 +31,15 @@ export class PerfilService {
         }
       })
     )
-    // this.perfilCollection = afs.collection<Perfil>('perfiles');
+    this.perfilCollection = afs.collection<Perfil>('perfiles');
+
+    this.loginService.user.subscribe(data =>{
+    this.usuarioLogeado = data.uid;
+
+    })
   }
+
+
 
   listar() {
     return this.afs.collection<Perfil>('perfiles').valueChanges();
@@ -75,6 +85,7 @@ export class PerfilService {
      capacidadRestaurante: perfil.capacidadRestaurante,
      horarioRestaurante: perfil.horarioRestaurante,
      direccionRestaurante: perfil.direccionRestaurante,
+     //imagenRes: perfil.imagen
     });
   }
 
@@ -94,43 +105,47 @@ export class PerfilService {
     return this.afs.collection('perfiles').doc(perfil.id).delete();
   }
 
-  // subirPerfilconImagen(perfiles: Perfil, image: FileI): void{
-  //   this.subirImagen(perfiles, image);
-  // }
+   subirPerfilconImagen(perfiles: Perfil, image: FileI): void{
+     this.subirImagen(perfiles, image);
+   }
 
-  // private guardarRestaurante(perfil: Perfil) {
-  //   const postObj = {
-  //     //id: perfil.id,
-  //     userUID: perfil.userUID,
-  //     nombreRestaurante: perfil.nombreRestaurante,
-  //     fotoRestaurante: perfil.fotoRestaurante,
-  //     tipoRestaurante: perfil.tipoRestaurante,
-  //     capacidadRestaurante: perfil.capacidadRestaurante,
-  //     horarioRestaurante: perfil.horarioRestaurante,
-  //     direccionRestaurante: perfil.direccionRestaurante,
-  //     fotoImg: this.UrlImagen,
-  //     fileRef: this.filePath
-  //   };
-  //     return this.perfilCollection.add(postObj);
-  // }
+  private guardarRestaurante(perfil: Perfil) {
+      let idPerfil = this.afs.createId(); 
+    const postObj = {
+
+       id: idPerfil,
+       userUID: this.usuarioLogeado,
+       nombreRestaurante: perfil.nombreRestaurante,
+       fotoRestaurante: perfil.fotoRestaurante,
+       tipoRestaurante: perfil.tipoRestaurante,
+       capacidadRestaurante: perfil.capacidadRestaurante,
+       horarioRestaurante: perfil.horarioRestaurante,
+       direccionRestaurante: perfil.direccionRestaurante,
+       imagenRes: this.UrlImagen,
+       fileRef: this.filePath
+     };
+        this.perfilCollection.add(postObj);
+   }
 
   
 
-  // private subirImagen(perfil: Perfil, image: FileI){
-  //   this.filePath = `imagenes/${image.name}`;
-  //   const fileRef = this.storage.ref(this.filePath);
-  //   const task = this.storage.upload(this.filePath, image);
-  //   task.snapshotChanges()
-  //     .pipe(
-  //       finalize(() => {
-  //         fileRef.getDownloadURL().subscribe(urlImage => {
-  //           this.UrlImagen = urlImage;
-  //           this.guardarRestaurante(perfil);
-  //         });
-  //       })
-  //     ).subscribe();
+    private subirImagen(perfil: Perfil ,image: FileI){
+     this.filePath = `imagenes/${image.name}`;
+     const fileRef = this.storage.ref(this.filePath);
+     const task = this.storage.upload(this.filePath, image);
+     task.snapshotChanges()
+       .pipe(
+         finalize(() => {
+          fileRef.getDownloadURL().subscribe(urlImage => {
+             this.UrlImagen = urlImage;
+             console.log('urlImagen', this.UrlImagen);
+             this.guardarRestaurante(perfil);
+             
+           });
+        })
+       ).subscribe();
 
-  // }
+   }
 
 
 
--- src/app/pages/crear-restaurante/crear-restaurante.component.ts
+++ src/app/pages/crear-restaurante/crear-restaurante.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild, ElementRef } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { ActivatedRoute, Router, Params } from '@angular/router';
 import { PerfilService } from '../../_service/perfil.service';
@@ -6,11 +6,12 @@ import { AngularFireStorage } from '@angular/fire/storage';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { MatSnackBar, MatDialog } from '@angular/material';
 import { LoginService } from '../../_service/login.service';
-import { Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
+import { Subject, Observable } from 'rxjs';
+import { takeUntil, finalize } from 'rxjs/operators';
 import { Perfil } from '../../_model/perfil';
 import Swal from 'sweetalert2';
 import { ModalComponent } from '../../modal/modal/modal.component';
+import { FileI } from '../../_model/imagenes';
 
 @Component({
   selector: 'app-crear-restaurante',
@@ -29,6 +30,11 @@ export class CrearRestauranteComponent implements OnInit, OnDestroy {
   labelFile: string;
   urlImage: string;
 
+  private filePath: any;
+  rutaImagen: Observable<string>;
+
+  
+
     // Se crear la variable para liberar recursos
     private ngUnsubscribe: Subject<void> = new Subject();
 
@@ -40,14 +46,16 @@ export class CrearRestauranteComponent implements OnInit, OnDestroy {
               private router: Router,
               private loginService: LoginService) { }
 
+
   public newPostForm = new FormGroup({
-    nombreRestaurante: new FormControl ('', Validators.required),
+    id: new FormControl (''),
+    nombreRestaurante: new FormControl ('', Validators.required),  
     fotoRestaurante: new FormControl('', Validators.required),
     tipoRestaurante: new FormControl('', Validators.required),
     capacidadRestaurante: new FormControl('', Validators.required),
     horarioRestaurante: new FormControl('', Validators.required),
     direccionRestaurante: new FormControl('', Validators.required),
-    fotoImg: new FormControl('', Validators.required)
+    imagePost: new FormControl('', Validators.required)
   });
 
   ngOnInit() {
@@ -73,12 +81,12 @@ export class CrearRestauranteComponent implements OnInit, OnDestroy {
     this.route.params.subscribe((params: Params) =>{
       this.id = params['id'];
       this.edicion = this.id != null;
-      this.initForm();
+      //this.initForm();
     });
 
     this.perfilService.listar().subscribe(data =>{
       this.perfil = data;
-      console.log(this.perfil)
+      console.log("perfil", this.perfil)
     });
 
     
@@ -96,12 +104,15 @@ export class CrearRestauranteComponent implements OnInit, OnDestroy {
           'direccionR': new FormControl(data.direccionRestaurante),
           'horarioR': new FormControl(data.horarioRestaurante),
           'capacidadR': new FormControl(data.capacidadRestaurante),
+          //'imagenPerfil': new FormControl(data.imagePost),
         });
         
         // Aqui no se usa el ngUnsubscribe porque se esta conectando con FireStorage
          if(data != null){
            this.afStorage.ref(`perfiles/${data.id}`).getDownloadURL().subscribe(data => {
             this.urlImage = data;
+            console.log("Es la imagen?? ", this.urlImage);
+            
            })
           }
       });
@@ -145,12 +156,15 @@ export class CrearRestauranteComponent implements OnInit, OnDestroy {
         }
     
 
-    let mensaje
+    //let mensaje
     if(this.edicion){
       this.perfilService.modificar(perfil);
       //Swal.fire('Eliminado!','Tu Restaurante ha sido eliminado','success')
     } else{
-      this.perfilService.registrar(perfil);   
+      this.perfilService.registrar(perfil);  
+     // let f =this.perfilService.subirImagen( this.file); 
+     // console.log("imagen>>", f);
+      
       Swal.fire('Agregado!','Tu Restaurante ha sido agregado','success')
     }
 
@@ -163,16 +177,38 @@ export class CrearRestauranteComponent implements OnInit, OnDestroy {
   }
 
 
-  // addPerfil(data: Perfil) {
-  //   console.log('New perfil', data);
-  //   this.perfilService.subirPerfilconImagen(data, this.file);
-  // }
+   addPerfil(data: Perfil) {
+     console.log('New perfil', data);
+    let perfil = new Perfil();
+    this.id = this.afs.createId();
+    perfil.id = this.id;
+    //perfil.userUID = this.usuarioLogeado;
+     this.perfilService.subirPerfilconImagen(data, this.file);
+     this.router.navigate(['editar']);
+   }
 
   // Funcion para mostrar el nombre del archivo seleccionado
   seleccionar(e: any): void{
     this.file = e.target.files[0];
-    //this.labelFile = e.target.files[0].name;
+    this.labelFile = e.target.files[0].name;
   }
+
+      //   task.snapshotChanges()
+      //   .pipe(
+      //    finalize(() => {
+      //     ref.getDownloadURL().subscribe(urlImage => {
+      //        this.urlImg = urlImage;
+      //        console.log('urlImagen', this.urlImg);
+      //        //perfil.fotoRes = this.UrlImagen;
+      //        //perfil.imagen = "http";
+      //      });
+      //   })
+      //  ).subscribe();
+
+    
+    //let f = this.perfilService.subirImagen(this.file);
+    //console.log("imagen??", f)
+  
 
   ngOnDestroy(){
     this.ngUnsubscribe.next();
